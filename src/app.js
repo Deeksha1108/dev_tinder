d78ff0f5839aa699f06af1b35706d19458d3c1b9
deeksha <deeksha1@appinventiv.com>
2026-02-25 17:19:12 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -14,7 +14,35 @@ const app = express();
 //   res.send("Hello from the server!");
 // });
 
+app.use(
+  "/users",
+  (req, res, next) => {
+    // It is called a dynamic routes
+    console.log("Handling the routes users");
+    // res.send("Response!!");    // Output is Response!! but Iss line ko comment krk fir yeh route hit krte h then yeh infinite loop me hii ghumta rhega next response pe nii jayega yhi infinite ghumta rhega jb tk aap next() nii lgaoge tb tk isiliye next() ka use kiya jata h kii hmara route/middleware infinite me na ghume
+    next(); // agr hum iss next() ko res.send se upr le jaye then yeh error dedega
+  },
+  (req, res) => {
+    console.log("Handling the route User 2!!");
+    res.send("2nd Response!!");
+    // next();
+  },
+  //   (req, res) => {
+  //     console.log("Handling the route User 3!!");
+  //     res.send("3rd Response!!");
+  //   },
+  //   (req, res) => {
+  //     console.log("Handling the route User 4!!");
+  //     res.send("4th Response!!");
+  //   },
+  //   (req, res) => {
+  //     console.log("Handling the route User 5!!");
+  //     res.send("5th Response!!");
+  //   },
+);
+
 app.get("/users/:userId/:name", (req, res) => {
+  // It is called a dynamic routes
   console.log(req.params);
   res.send({ firstName: "Deeksha", lastName: "Singh" });
 });
@@ -27,7 +55,8 @@ app.delete("/users", (req, res) => {
   res.send("Data deleted successfully");
 });
 
-app.use("/test", (req, res) => {  // request handler
+app.use("/test", (req, res) => {
+  // request handler
   res.send("Bla Bla Bla!");
 });
 
